8e631e3ba13a19be02dd32a7ce716e15cbd6d88a
Fhel Jhon Feliciano <fhelfeliciano@gmail.com>
2026-07-17 13:10:53 +0800

    jb name

--- scripts/generate-static-index.js
+++ scripts/generate-static-index.js
@@ -18,7 +18,7 @@ const html = `<!DOCTYPE html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Fhel Jhon Feliciano | Portfolio</title>
+        <title>Jaybhee P. Dahay | Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
